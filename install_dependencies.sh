071812bcc69d0e08a24c9da0bdad5cc6afe1459b
A55AF <mahmoudassaf19@gmail.com>
2026-09-12 00:29:04 +0300

    add install_dependencies.sh: detect package manager

---
+++ install_dependencies.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ -f /etc/os-release ]]; then
+	source /etc/os-release
+else
+	echo "error: file not found"
+	exit 1
+fi
+
+DISTRO=$ID
+PACKAGE_MANAGER=""
+if [[ $DISTRO == "ubuntu" ]] || [[ $DISTRO == "debian" ]]; then
+	PACKAGE_MANAGER="apt"
+elif [[ $DISTRO == "fedora" ]]; then
+	PACKAGE_MANAGER="dnf"
+elif [[ $DISTRO == "arch" ]]; then
+	PACKAGE_MANAGER="pacman"
+fi
+
+for par in $ID_LIKE; do
+	case $par in
+		arch)
+			PACKAGE_MANAGER="pacman"
+			break
+			;;
+		ubuntu|debian)
+			PACKAGE_MANAGER="apt"
+			break
+			;;
+		fedora|rhel)
+			PACKAGE_MANAGER="dnf"
+			break
+			;;
+	esac
+done
+
+echo $PACKAGE_MANAGER
