a6681aec7bf8629c4de14fe67bcecadcd91f01ac
A55AF <mahmoudassaf19@gmail.com>
2026-09-12 00:55:37 +0300

    update install_dependencies.sh: refactor it with functions

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-if [[ -f /etc/os-release ]]; then
-	source /etc/os-release
-else
-	echo "error: file not found"
-	exit 1
-fi
 
-DISTRO=$ID
-PACKAGE_MANAGER=""
-if [[ $DISTRO == "ubuntu" ]] || [[ $DISTRO == "debian" ]]; then
-	PACKAGE_MANAGER="apt"
-elif [[ $DISTRO == "fedora" ]]; then
-	PACKAGE_MANAGER="dnf"
-elif [[ $DISTRO == "arch" ]]; then
-	PACKAGE_MANAGER="pacman"
-fi
+detect_package_manager() {
+	if [[ -f /etc/os-release ]]; then
+		source /etc/os-release
+	else
+		echo "error: file not found"
+		exit 1
+	fi
 
-for par in $ID_LIKE; do
-	case $par in
-		arch)
-			PACKAGE_MANAGER="pacman"
-			break
-			;;
-		ubuntu|debian)
-			PACKAGE_MANAGER="apt"
-			break
-			;;
-		fedora|rhel)
-			PACKAGE_MANAGER="dnf"
-			break
-			;;
-	esac
-done
+	DISTRO=$ID
+	PACKAGE_MANAGER=""
+	if [[ $DISTRO == "ubuntu" ]] || [[ $DISTRO == "debian" ]]; then
+		PACKAGE_MANAGER="apt"
+	elif [[ $DISTRO == "fedora" ]]; then
+		PACKAGE_MANAGER="dnf"
+	elif [[ $DISTRO == "arch" ]]; then
+		PACKAGE_MANAGER="pacman"
+	fi
 
-echo $PACKAGE_MANAGER
+	for par in $ID_LIKE; do
+		case $par in
+			arch)
+				PACKAGE_MANAGER="pacman"
+				break
+				;;
+			ubuntu|debian)
+				PACKAGE_MANAGER="apt"
+				break
+				;;
+			fedora|rhel)
+				PACKAGE_MANAGER="dnf"
+				break
+				;;
+		esac
+	done
+
+	echo $PACKAGE_MANAGER
+}
